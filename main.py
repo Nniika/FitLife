fab8ad765369b20cc3eff1a9814ed12a48ceb5ca
Nika <LA.N21@yandex.ru>
2026-06-23 19:14:28 +0300

    Добавила защиту от ошибок

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ print("Привет! Я фитнес-бот!")
 print("Давай познакомимся!")
 
 user_name = input("Как тебя зовут? ")
+
 while True:
     try:
         user_age = int(input("Сколько тебе лет? "))
@@ -11,21 +12,27 @@ while True:
 
 while True:
     try:
-        user_weight = float(input("Какой у тебя вес (в кг)? Используй точку, например: 75.5 "))
+        user_weight = float(input(
+            "Какой у тебя вес (в кг)? Используй точку, например: 75.5 "
+        ))
         break
     except ValueError:
         print("Ошибка! Введите число с точкой, например: 75.5")
 
 while True:
     try:
-        user_height = float(input("Какой у тебя рост (в метрах)? Используй точку, например: 1.75 "))
+        user_height = float(input(
+            "Какой у тебя рост (в метрах)? Используй точку, например: 1.75 "
+        ))
         break
     except ValueError:
-        print("Ошибка! Введите число с точкой, например: 1.75") # Я написала с помощью ИИ, я еще очень плохо понимаю куда что применять надо((
+        print("Ошибка! Введите число с точкой, например: 1.75")
 
+# Расчёт индекса массы тела
 bmi = user_weight / (user_height ** 2)
 bmi_rounded = round(bmi, 1)
 
+# Расчёт нормы воды
 water_ml = user_weight * 30
 water_l = round(water_ml / 1000, 2)
 
@@ -38,3 +45,5 @@ print(f"ИМТ: {bmi_rounded}")
 print(f"Норма воды: {water_l} литра в день")
 print("=" * 40)
 print("Расчет окончен. Будьте здоровы!")
+
+# Мне помог ИИ, так как я ещё плохо ориентируюсь в коде
