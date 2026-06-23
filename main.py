b456bd500db4f4b061c7767a91165a8ec795207e
Nika <LA.N21@yandex.ru>
2026-06-23 19:07:51 +0300

    Исправлен код

--- main.py
+++ main.py
@@ -2,10 +2,26 @@ print("Привет! Я фитнес-бот!")
 print("Давай познакомимся!")
 
 user_name = input("Как тебя зовут? ")
-user_age = int(input("Сколько тебе лет? "))
+while True:
+    try:
+        user_age = int(input("Сколько тебе лет? "))
+        break
+    except ValueError:
+        print("Ошибка! Введите число, например: 25")
 
-user_weight = float(input("Какой у тебя вес (в кг)? "))
-user_height = float(input("Какой у тебя рост (в метрах)? Например, 1.75: "))
+while True:
+    try:
+        user_weight = float(input("Какой у тебя вес (в кг)? Используй точку, например: 75.5 "))
+        break
+    except ValueError:
+        print("Ошибка! Введите число с точкой, например: 75.5")
+
+while True:
+    try:
+        user_height = float(input("Какой у тебя рост (в метрах)? Используй точку, например: 1.75 "))
+        break
+    except ValueError:
+        print("Ошибка! Введите число с точкой, например: 1.75") # Я написала с помощью ИИ, я еще очень плохо понимаю куда что применять надо((
 
 bmi = user_weight / (user_height ** 2)
 bmi_rounded = round(bmi, 1)
